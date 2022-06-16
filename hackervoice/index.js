2e9b6335a9f25f5fdd743641ab7bcd0e44276295
Noah Zhang <zhangh21@usc.edu>
2022-06-16 16:48:01 -0400

    week1 step5

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -1,10 +1,19 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const password = req.query.password
+    const password = req.query.password;
+    let response;
+    if (password == "letmein")
+    {
+        response = "Access granted";
+    }
+    else 
+    {
+        response = "Access denied";
+    }
     
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: response
     };
 }
