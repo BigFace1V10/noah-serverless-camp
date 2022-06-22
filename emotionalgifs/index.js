18c0e15b8fecce56eef47ed5cf63c50c024c5ffe
Noah Zhang <zhangh21@usc.edu>
2022-06-22 17:03:15 -0400

    2.3 [draft]

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -6,10 +6,36 @@ module.exports = async function (context, req) {
     const boundary = multipart.getBoundary(req.headers['content-type']);
     const body = req.body;
     const parts = multipart.Parse(body, boundary);
-    let convertedResult = Buffer.from(parts[0].data).toString('base64');
-
+    const result = await analyzeImage(parts[0].data);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
+        body: {
+            result
+        }
     };
+    console.log(result)
+    context.done(); 
+}
+
+async function analyzeImage(img){
+    const subscriptionKey = 'd94922ac7b7744e4b6ff15775e4f52f0';
+    const uriBase = 'https://noahsfaceapi.cognitiveservices.azure.com/face/v1.0/detect';
+
+    // specify the parameters for your request
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'
+    })
+
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',  //WHAT TYPE OF REQUEST?
+        body: 'img',  //WHAT ARE WE SENDING TO THE API?
+      	//ADD YOUR TWO HEADERS HERE
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    })
+    let data = await resp.json();
+
+    return data;
 }
