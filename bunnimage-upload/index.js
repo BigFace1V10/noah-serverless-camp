d104fa8d8740d6826cef5a5941322ba025af3b07
Noah Zhang <zhangh21@usc.edu>
2022-07-01 12:32:08 -0400

    3.3 update

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -47,7 +47,7 @@ async function uploadFile(parsedBody, ext, password)
     const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container 
     
     // Create a blob
-    const blobName = 'test.' + ext;    // Create the container
+    const blobName = password + '.' + ext;    // Create the container
     const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
 
     // Upload data to blob
