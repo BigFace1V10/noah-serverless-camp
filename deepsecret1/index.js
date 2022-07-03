22d36f7705072777b41c5779870e1ae5e4a0e36b
Noah Zhang <zhangh21@usc.edu>
2022-07-03 15:48:58 -0400

    3.6 deepest scret api

---
+++ deepsecret1/index.js
@@ -0,0 +1,8 @@
+const querystring = require("qs")
+
+module.exports = async function (context, req) {
+    const queryObject = querystring.parse(req.body);
+    context.res = {
+        body: queryObject.Body
+    };
+}
