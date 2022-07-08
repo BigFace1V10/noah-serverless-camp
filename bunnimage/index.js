610fe9e9db154995a47ce3c2b9fea48e20f1bc38
Noah Zhang <zhangh21@usc.edu>
2022-07-08 12:30:29 -0400

    4.2 add file upload

--- bunnimage/index.js
+++ bunnimage/index.js
@@ -3,6 +3,19 @@ const bunnForm = document.getElementById('bunnForm');
 bunnForm.addEventListener('submit', function (event) {
     event.preventDefault() // prevent reloading
     const username = document.getElementById("username").value
+    if (username == "") {
+        alert("No name error.")
+    }
     const output = document.getElementById("output")
-    output.textContent = username + "❤"
+    output.textContent = "Thanks!"
   }); // listen to event type called "submit"; get value of text box
+
+
+// bunnForm.addEventListener('change', function (event) {
+//     const username = document.getElementById("username").value
+//     const filename = username.split(/(\\|\/)/g).pop()
+//     var re = /(\.jpg|\.jpeg|\.bmp|\.gif|\.png)$/i;
+//     if (!re.exec(filename)) {
+//         alert("File extension not supported!");
+//     }
+// });
